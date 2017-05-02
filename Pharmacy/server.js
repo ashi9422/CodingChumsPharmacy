fdad945eec303c0a03501241945f01d09e34578f
Ashani Madhushika <Ashanikulasinghe@gmail.com>
2017-05-02 19:58:55 +0530

    new template and public folder

--- Pharmacy/server.js
+++ Pharmacy/server.js
@@ -8,6 +8,21 @@ const app = express();
 
 app.use(bodyParser.json());
 
+mongoose.connect('mongodb://ashani:ashani123@ds011902.mlab.com:11902/userstory12', err => {
+    if (err) {
+        console.log(err);
+        //process.exit(1);
+    }
+    else {
+        console.log('connect to the database');
+    }
+});
+
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
 app.listen(3000, function (err) {
     if(err){
         console.log(err);
